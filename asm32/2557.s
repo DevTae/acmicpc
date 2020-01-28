6c5f77ebc6d839188296c811b0b426d053539aa6
ahdelron <fmkms@naver.com>
2020-01-29 03:06:23 +0900

    eax register must have a return value.

--- asm32/2557.s
+++ asm32/2557.s
@@ -1,17 +1,35 @@
+; 1번째 방법 : printf 이용
 section .data
-	msg db "Hello World!"
+	data : db "Hello World!", 0xA, 0
+	extern printf
 
 section .text
-	global _start
+	global main
 
-_start:
-	mov eax, 4 ; sys_write
-	mov ebx, 1 ; 출력 모드
-	mov ecx, msg
-	mov edx, 12
-	int 0x80 ; syscall 와 같음 32비트 컴파일 시에는 int 0x80 사용해야 함.
+main:
+	push ebp
+	mov ebp, esp
+	push data
+	call printf
+	add esp, 4
+	mov eax, 0 ; 레지스터 eax에 리턴 값 0을 집어 넣어줌.
+	leave
+	ret
 
-end:
-	mov eax, 1 ; sys_exit
-	mov ebx, 0
-	int 0x80 ; 위와 마찬가지
+; 2번째 방법 : 시스템콜 이용
+;section .data
+;	msg : db "Hello World!"
+;
+;section .text
+;	global main
+;
+;main:
+;	push ebp
+;	mov ebp, esp
+;	mov eax, 4 ; sys_write
+;	mov ebx, 1 ; 출력 모드
+;	mov ecx, msg
+;	mov edx, 12
+;	int 0x80 ; syscall 와 같음 32비트 컴파일 시에는 int 0x80 사용해야 함.
+;	leave
+;	ret
